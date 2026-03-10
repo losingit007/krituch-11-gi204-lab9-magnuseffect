5e32eed31aa6bf2fce1b0d1cd8b6ebe7ac95b82b
losingit007 <krituch.rakk@bumail.net>
2026-03-10 10:02:24 +0700

    AngularVelocityCube & TorqueCube

---
+++ Assets/Scripts/TorqueRotate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueRotate : MonoBehaviour
+{
+    [SerializeField] public float torque;
+
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(Vector3.right *  torque);
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
